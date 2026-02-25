709154fac5056acf4773352ced1b38e6be2f4445
Nick Yianilos <nyianilos@collegeboard.org>
2026-02-25 08:46:04 -0500

    generated onboard ui skeleton

---
+++ packages/domains/onboard/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.scss';
+
+export function BawpUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to BawpUi!</h1>
+    </div>
+  );
+}
+
+export default BawpUi;
